1e1259b9470f63bc583b0cb869d31a94f896460c
vinetailgames <vinetailstudios@gmail.com>
2026-09-02 11:14:28 -0500

    F: added a new constant to constants.py and added the move method to main.py

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ from circleshape import CircleShape
 from constants import PLAYER_RADIUS
 from constants import LINE_WIDTH
 from constants import PLAYER_TURN_SPEED
+from constants import PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -22,6 +23,12 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
     
     def update(self, dt:float) -> None:
         keys = pygame.key.get_pressed()
@@ -29,3 +36,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.move(-dt)
