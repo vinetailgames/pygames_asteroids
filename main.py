d5e39fc5ba1268cf9459a9cf3317169dbbcc6b67
vinetailgames <vinetailstudios@gmail.com>
2026-09-09 12:31:04 -0500

    G: updated main.py to include groups

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
     while True:
@@ -21,10 +26,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0
-        player.update(dt)
+        updatable.update(dt)
 
 
 if __name__ == "__main__":
