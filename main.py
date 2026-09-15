e256f66fe2a40e45a06aef037fe1ffc1ad4dc0a5
vinetailgames <vinetailstudios@gmail.com>
2026-09-14 19:39:55 -0500

    I: implemented collision for the ship and outputs game over message when asteroid collides with ship

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from logger import log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+import sys
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -37,6 +39,12 @@ def main():
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0
         updatable.update(dt)
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+                
 
 
 if __name__ == "__main__":
