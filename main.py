17c636557fbe2a1469f87aebb2a7eda105574816
vinetailgames <vinetailstudios@gmail.com>
2026-09-16 18:42:16 -0500

    L: Implemented kill method for when bullet strikes asteroid

--- main.py
+++ main.py
@@ -48,6 +48,12 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
                 
 
 
